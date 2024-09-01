628254e266054487d3a56a46c6d24c3a26c095fe
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-09-01 13:23:37 +0300

    Book model implementation

---
+++ src/main/java/org/example/bookshop/model/Book.java
@@ -0,0 +1,43 @@
+package org.example.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    private String coverImage;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String isbn, BigDecimal price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+}
