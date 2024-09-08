e0aaf0421307263068bdf5a535aa7b62c5b45e88
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-09-08 19:03:38 +0300

    Book storage solution

---
+++ src/main/java/org/example/bookshop/dto/CreateBookRequestDto.java
@@ -0,0 +1,21 @@
+package org.example.bookshop.dto;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateBookRequestDto {
+    private String title;
+
+    private String author;
+
+    private BigDecimal price;
+
+    private String isbn;
+
+    private String description;
+
+    private String coverImage;
+}
