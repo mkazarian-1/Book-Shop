b7dfb303b1992723760a587e9f5dd387fca134b9
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-10-09 17:19:24 +0300

    JWT authorization implementation (maven fix)

--- src/main/java/org/example/bookshop/security/JwtUtil.java
+++ src/main/java/org/example/bookshop/security/JwtUtil.java
@@ -1,16 +1,16 @@
 package org.example.bookshop.security;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.function.Function;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
