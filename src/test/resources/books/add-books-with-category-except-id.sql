f43fa34b26025f742e2aa790e6889d67d93e1116
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-12-05 18:17:24 +0200

    Book and Category services test impl

---
+++ src/test/resources/books/add-books-with-category-except-id.sql
@@ -0,0 +1,11 @@
+insert into categories (is_deleted, name, description) values ( false, 'lol info','some description');
+insert into categories (is_deleted, name, description) values ( false, 'lol info1','some description');
+
+insert into books (is_deleted, title, isbn, price, author) values (false,'Some Title1','12345', 100, 'Jhon kik 1');
+insert into books (is_deleted, title, isbn, price, author) values (false,'Some Title2','12346', 120, 'Jhon kik 2');
+insert into books (is_deleted, title, isbn, price, author) values (false,'Title3','12347', 130, 'Jhon kik 3');
+insert into books (is_deleted, title, isbn, price, author) values (false,'Title4','12348', 140, 'Jhon kik 4');
+
+insert into books_categories (book_id, categories_id) values (1,1);
+insert into books_categories (book_id, categories_id) values (1,2);
+insert into books_categories (book_id, categories_id) values (2,1);
---
+++ src/test/resources/categories/add-categories-without-id.sql
@@ -0,0 +1,4 @@
+insert into categories (is_deleted, name, description) values (false, 'lol info1','some description1');
+insert into categories (is_deleted, name, description) values (false, 'lol info2','some description2');
+insert into categories (is_deleted, name, description) values (false, 'lol info3','some description3');
+insert into categories (is_deleted, name, description) values (false, 'lol info4','some description4');
